69a21629c4d81c377a9b934d5d3de1c1bc529e36
Gianni <sb5021087@gmail.com>
2023-11-29 04:20:12 -0500

    view add element

--- src/app/Module/feature/components/product-details/product-details.component.ts
+++ src/app/Module/feature/components/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { lehngacholiPage2 } from 'src/Data/Saree/lenghaCholiPage2';
 
 @Component({
@@ -10,10 +11,15 @@ export class ProductDetailsComponent {
 selectedSize:any;
 reviews=[1,1,1];
 relatedProducts :any;
+constructor(private router: Router){
+
+}
 ngOnInit(){
   this.relatedProducts= lehngacholiPage2;
 }
 handleAddToCart(){
   console.log("selected size ", this.selectedSize)
+
+  this.router.navigate(['cart'])
 }
 }
--- src/app/Module/shared/shared.module.ts
+++ src/app/Module/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { StartRatingComponent } from './components/start-rating/start-rating.component';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
 
 
 
@@ -17,7 +18,8 @@ import { StartRatingComponent } from './components/start-rating/start-rating.com
     FooterComponent,
     NavContentComponent,
     ProductCardComponent,
-    StartRatingComponent
+    StartRatingComponent,
+    CartItemComponent
    
   ],
   imports: [
@@ -30,7 +32,8 @@ import { StartRatingComponent } from './components/start-rating/start-rating.com
     NavbarComponent,
     FooterComponent,
     ProductCardComponent,
-    StartRatingComponent
+    StartRatingComponent,
+    CartItemComponent
   ]
 })
 export class SharedModule { }
