4216a78991b97dbc40dbf6cefb6bd2c6b87e06c2
Gianni <sb5021087@gmail.com>
2023-11-29 16:07:52 -0500

    change image

--- src/Data/mainCarousel.ts
+++ src/Data/mainCarousel.ts
@@ -1,24 +1,11 @@
 
 export const homeCarouselData =[
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+        image:"https://static.runnea.com/images/202209/las-sneakers-mas-deseadas-2022-1200x572x80xX.jpg?1",
         path:"/women/cloting/lengha_choli"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        image:"https://media.revistagq.com/photos/5ca5f975b73808e4ff819508/16:9/w_1920,c_limit/zapatillas_basicas_para_hombre_moda_tendencias_2831.jpg",
         path:"/women/cloting/women_dress"
-    },
-    {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/7/17_-_copy-1.jpg",
-        path: "/women/cloting/women_dress"
-    },
-    {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/7/17_-_copy-1.jpg",
-        path: "/women/cloting/women_dress"
-    },
-    {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/7/17_-_copy-1.jpg",
-        path: "/women/cloting/women_dress"
     }
-   
 ] 
